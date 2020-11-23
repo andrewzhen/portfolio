5c77844c4b736adba1229211daeeca527c6ce2cf
andrewzhen <ajzhen@ucsd.edu>
2020-11-23 14:45:03 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Header from "./components/Header";
+// import Main from "./components";
+// import Footer from "./components";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <Main />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
